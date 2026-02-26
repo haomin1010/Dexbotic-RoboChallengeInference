61981a4d1159b607ae3c17a9ccb2cf3ac9966bd6
haomin1010 <lihaomin1010@gmail.com>
2026-02-26 15:33:03 +0800

    arx5 client

--- spirit/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Local ARX5 execution for Spirit VLA: run policy locally, execute on local arm."""
-
-from .arm_client import ARX5ArmClient
-from .local_loop import local_control_loop
-from .local_robot_interface import LocalRobotInterface
-
-__all__ = ["ARX5ArmClient", "LocalRobotInterface", "local_control_loop"]
